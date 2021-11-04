49ee8bbcb0c2e86c98894b24f2fa15648eb597d1
zavadskivadzim <kp002210@mail.ru>
2021-11-04 22:08:23 +0300

    multimodule structure (model)

---
+++ model/src/main/java/com/epam/brest/Player.java
@@ -0,0 +1,52 @@
+package com.epam.brest;
+
+import java.time.LocalDate;
+
+public class Player {
+
+    private Integer playerId;
+    private String name;
+    private String surname;
+    private LocalDate dateOfBirthday;
+    private Integer teamId;
+
+    public Integer getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Integer playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfBirthday() {
+        return dateOfBirthday;
+    }
+
+    public void setDateOfBirthday(LocalDate dateOfBirthday) {
+        this.dateOfBirthday = dateOfBirthday;
+    }
+
+    public Integer getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Integer teamId) {
+        this.teamId = teamId;
+    }
+}
---
+++ model/src/main/java/com/epam/brest/Team.java
@@ -0,0 +1,23 @@
+package com.epam.brest;
+
+public class Team {
+
+    private Integer teamId;
+    private String teamName;
+
+    public Integer getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Integer teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+}
