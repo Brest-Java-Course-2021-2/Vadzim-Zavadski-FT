9c8d91360419f08ac08bb8e31ed2091a21a1b9d7
zavadskivadzim <kp002210@mail.ru>
2022-01-11 21:29:56 +0300

    add <mainClass> to pom.xml

--- web-application/src/main/java/com/zavadski/FtApplication.java
+++ web-application/src/main/java/com/zavadski/FtApplication.java
@@ -1,9 +1,11 @@
-package com.zavadski.web_app;
+package com.zavadski;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:dao.properties"})
 public class FtApplication {
 
     public static void main(String[] args) {
